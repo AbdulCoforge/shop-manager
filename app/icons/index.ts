8a82e1a83764408b5cfcc74ed4b4a921d5335e22
Abdul Rehman <abdulrehman@Abduls-Mac-mini.local>
2026-01-25 14:15:16 +0530

    icons setuped

---
+++ app/icons/index.ts
@@ -0,0 +1,10 @@
+export { HiArrowTrendingUp } from "react-icons/hi2";
+export { FaPlus } from "react-icons/fa6";
+export { LuCircleCheckBig } from "react-icons/lu";
+export { RiUser3Line } from "react-icons/ri";
+export { CiCalendar } from "react-icons/ci";
+export { RxSpeakerLoud } from "react-icons/rx";
+export { BsBoxSeam } from "react-icons/bs";
+export { IoIosMic } from "react-icons/io";
+
+
